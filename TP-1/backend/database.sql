2e940dfe1e722180f1c2db1266da2e6a6dbdca63
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2024-11-11 21:31:21 +0100

    Update database.sql

--- TP-1/backend/database.sql
+++ TP-1/backend/database.sql
@@ -1,7 +1,6 @@
 CREATE TABLE book (title character varying(100) NOT NULL);
 INSERT INTO book (title)
-VALUES ('DevOps'),
-    ('ATTIA Noah'),
+VALUES ('ATTIA Noah'),
     ('CHAZARD Elisa'),
     ('CLAVIER Guillaume'),
     ('FAURE Alexis');
